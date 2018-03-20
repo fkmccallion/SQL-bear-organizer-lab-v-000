326805293ac50369992a74483da194af5d0f5e8f
Frank McCallion <fkmccallion@gmail.com>
2018-03-20 12:04:55 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY color HAVING COUNT(color) == 3" #Come_back_to_this
+  "SELECT color, COUNT(color) FROM bears GROUP BY color" #Come_back_to_this
 end
 
 def counts_number_of_bears_with_goofy_temperaments
